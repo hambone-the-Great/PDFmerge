2207d20989cbd598f1b5252d583ad1f10b288413
hambone-the-Great <marcus.schuff@gmail.com>
2019-05-29 23:45:46 -0500

    sweetness

--- PDFmerge/Main.Designer.cs
+++ PDFmerge/Main.Designer.cs
@@ -28,47 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // listBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(54, 53);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(10);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(627, 27);
-            this.textBox1.TabIndex = 0;
+            this.listBox1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(463, 327);
+            this.listBox1.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.listBox1, "Drag and Drop PDF files here. ");
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
-            // button1
+            // btnUp
             // 
-            this.button1.Location = new System.Drawing.Point(698, 53);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 27);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnUp.BackColor = System.Drawing.SystemColors.Control;
+            this.btnUp.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnUp.FlatAppearance.BorderSize = 0;
+            this.btnUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUp.Image = ((System.Drawing.Image)(resources.GetObject("btnUp.Image")));
+            this.btnUp.Location = new System.Drawing.Point(481, 97);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(87, 81);
+            this.btnUp.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.btnUp, "Move Up");
+            this.btnUp.UseVisualStyleBackColor = false;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.BackColor = System.Drawing.SystemColors.Control;
+            this.btnDown.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnDown.FlatAppearance.BorderSize = 0;
+            this.btnDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDown.Image = ((System.Drawing.Image)(resources.GetObject("btnDown.Image")));
+            this.btnDown.Location = new System.Drawing.Point(481, 184);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(87, 81);
+            this.btnDown.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.btnDown, "Move Down");
+            this.btnDown.UseVisualStyleBackColor = false;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.BackColor = System.Drawing.SystemColors.Control;
+            this.btnMerge.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnMerge.FlatAppearance.BorderSize = 0;
+            this.btnMerge.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMerge.Image = ((System.Drawing.Image)(resources.GetObject("btnMerge.Image")));
+            this.btnMerge.Location = new System.Drawing.Point(481, 271);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(87, 81);
+            this.btnMerge.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.btnMerge, "Merge");
+            this.btnMerge.UseVisualStyleBackColor = false;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.Control;
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnClear.Enabled = false;
+            this.btnClear.FlatAppearance.BorderSize = 0;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Image = ((System.Drawing.Image)(resources.GetObject("btnClear.Image")));
+            this.btnClear.Location = new System.Drawing.Point(481, 10);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(87, 81);
+            this.btnClear.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.btnClear, "Delete Selected File");
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(867, 497);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(576, 372);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnMerge);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.listBox1);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
             this.Name = "Main";
             this.Text = "PDF Merge";
+            this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
--- PDFmerge/Main.cs
+++ PDFmerge/Main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using VB = Microsoft.VisualBasic;
+using System.Diagnostics;
+
 
 namespace PDFmerge
 {
@@ -15,6 +22,185 @@ namespace PDFmerge
         public Main()
         {
             InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(MainForm_DragEnter);
+            this.DragDrop += new DragEventHandler(MainForm_DragDrop);
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+
+                FileInfo info = new FileInfo(file);
+
+                if (info.Extension == ".pdf")
+                {
+                    listBox1.Items.Add(file);
+                }
+            }
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            MoveItem(-1);
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            MoveItem(1);
+        }
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            IEnumerable<string> files = listBox1.Items.Cast<string>();
+
+            string[] allDirs = new string[listBox1.Items.Count]; 
+            string targetDir = string.Empty;
+            int i = 0; 
+            foreach (string file in files)
+            {
+                FileInfo info = new FileInfo(file);
+                allDirs[i] = info.Directory.FullName;
+                i++;
+            }
+
+            targetDir = allDirs[0];
+
+            string prompt = "What do you want to name the merged PDF file? \r\nThe merged PDF file will be saved in: \r\n" + targetDir;
+
+            string newFileName = VB.Interaction.InputBox(prompt, "PDF File Name", "Merged_PDF_Doc_" + DateTime.Now.Month + "_" + DateTime.Now.Day + "_" + DateTime.Now.Year + "_" + DateTime.Now.Hour + "_" + DateTime.Now.Minute + ".pdf");
+
+           
+            if (!string.IsNullOrEmpty(newFileName))
+            {
+                newFileName = newFileName.Contains(".pdf") ? newFileName : newFileName + ".pdf";
+
+                string fullPath = Path.Combine(targetDir, newFileName);
+
+                bool success = MergePDFs(files, fullPath);
+
+                Application.DoEvents();
+
+                if (success)
+                {
+                    //Process.Start(@"" + targetDir);
+                    Process.Start(@"" + fullPath);
+                    listBox1.Items.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Something went wrong. Close the program and try again.");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Merge failed. No file name provided.");
+            }
+        }
+
+        public void MoveItem(int direction)
+        {
+            // Checking selected item
+            if (listBox1.SelectedItem == null || listBox1.SelectedIndex < 0)
+                return; // No selected item - nothing to do
+
+            // Calculate new index using move direction
+            int newIndex = listBox1.SelectedIndex + direction;
+
+            // Checking bounds of the range
+            if (newIndex < 0 || newIndex >= listBox1.Items.Count)
+                return; // Index out of range - nothing to do
+
+            object selected = listBox1.SelectedItem;
+
+            // Removing removable element
+            listBox1.Items.Remove(selected);
+            // Insert it in new position
+            listBox1.Items.Insert(newIndex, selected);
+            // Restore selection
+            listBox1.SetSelected(newIndex, true);
+        }
+
+
+        public static bool MergePDFs(IEnumerable<string> fileNames, string targetPdf)
+        {
+            bool merged = true;
+            using (FileStream stream = new FileStream(targetPdf, FileMode.Create))
+            {
+                Document document = new Document();
+                PdfCopy pdf = new PdfCopy(document, stream);
+                PdfReader reader = null;
+                try
+                {
+                    document.Open();
+                    foreach (string file in fileNames)
+                    {
+                        reader = new PdfReader(file);
+                        pdf.AddDocument(reader);
+                        reader.Close();
+                    }
+                }
+                catch (Exception)
+                {
+                    merged = false;
+                    if (reader != null)
+                    {
+                        reader.Close();
+                    }
+                }
+                finally
+                {
+                    if (document != null)
+                    {
+                        document.Close();
+                    }
+                }
+            }
+            return merged;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int selected = listBox1.SelectedIndex;
+
+                if (listBox1.Items.Count > 0 && selected > -1)
+                {
+                    listBox1.Items.Remove(listBox1.Items[selected]);
+                    listBox1.SelectedIndex = selected > 0 ? selected - 1 : selected;
+                }
+                else
+                {
+                    btnClear.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                btnClear.Enabled = true;
+            }
         }
     }
 }

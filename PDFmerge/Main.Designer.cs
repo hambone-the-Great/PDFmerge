0dbd52afe3678c01064bf1d126b6d9e9e9077707
hambone-the-Great <marcus.schuff@gmail.com>
2022-02-25 17:45:07 -0800

    layout changes

--- PDFmerge/Main.Designer.cs
+++ PDFmerge/Main.Designer.cs
@@ -51,21 +51,22 @@
             this.listBox1.Name = "listBox1";
             this.tableLayoutPanel1.SetRowSpan(this.listBox1, 4);
             this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(560, 254);
+            this.listBox1.Size = new System.Drawing.Size(955, 286);
             this.listBox1.TabIndex = 0;
             this.toolTip1.SetToolTip(this.listBox1, "Drag and Drop PDF files here. ");
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // btnUp
             // 
+            this.btnUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.btnUp.BackColor = System.Drawing.SystemColors.Control;
             this.btnUp.Cursor = System.Windows.Forms.Cursors.Default;
             this.btnUp.FlatAppearance.BorderSize = 0;
             this.btnUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnUp.Image = ((System.Drawing.Image)(resources.GetObject("btnUp.Image")));
-            this.btnUp.Location = new System.Drawing.Point(569, 68);
+            this.btnUp.Location = new System.Drawing.Point(964, 76);
             this.btnUp.Name = "btnUp";
-            this.btnUp.Size = new System.Drawing.Size(87, 59);
+            this.btnUp.Size = new System.Drawing.Size(116, 67);
             this.btnUp.TabIndex = 1;
             this.toolTip1.SetToolTip(this.btnUp, "Move Up");
             this.btnUp.UseVisualStyleBackColor = false;
@@ -73,14 +74,15 @@
             // 
             // btnDown
             // 
+            this.btnDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.btnDown.BackColor = System.Drawing.SystemColors.Control;
             this.btnDown.Cursor = System.Windows.Forms.Cursors.Default;
             this.btnDown.FlatAppearance.BorderSize = 0;
             this.btnDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDown.Image = ((System.Drawing.Image)(resources.GetObject("btnDown.Image")));
-            this.btnDown.Location = new System.Drawing.Point(569, 133);
+            this.btnDown.Location = new System.Drawing.Point(964, 149);
             this.btnDown.Name = "btnDown";
-            this.btnDown.Size = new System.Drawing.Size(87, 59);
+            this.btnDown.Size = new System.Drawing.Size(116, 67);
             this.btnDown.TabIndex = 2;
             this.toolTip1.SetToolTip(this.btnDown, "Move Down");
             this.btnDown.UseVisualStyleBackColor = false;
@@ -88,14 +90,15 @@
             // 
             // btnMerge
             // 
+            this.btnMerge.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.btnMerge.BackColor = System.Drawing.SystemColors.Control;
             this.btnMerge.Cursor = System.Windows.Forms.Cursors.Default;
             this.btnMerge.FlatAppearance.BorderSize = 0;
             this.btnMerge.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMerge.Image = ((System.Drawing.Image)(resources.GetObject("btnMerge.Image")));
-            this.btnMerge.Location = new System.Drawing.Point(569, 198);
+            this.btnMerge.Location = new System.Drawing.Point(964, 222);
             this.btnMerge.Name = "btnMerge";
-            this.btnMerge.Size = new System.Drawing.Size(87, 59);
+            this.btnMerge.Size = new System.Drawing.Size(116, 67);
             this.btnMerge.TabIndex = 3;
             this.toolTip1.SetToolTip(this.btnMerge, "Merge");
             this.btnMerge.UseVisualStyleBackColor = false;
@@ -103,15 +106,16 @@
             // 
             // btnClear
             // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.btnClear.BackColor = System.Drawing.SystemColors.Control;
             this.btnClear.Cursor = System.Windows.Forms.Cursors.Default;
             this.btnClear.Enabled = false;
             this.btnClear.FlatAppearance.BorderSize = 0;
             this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClear.Image = ((System.Drawing.Image)(resources.GetObject("btnClear.Image")));
-            this.btnClear.Location = new System.Drawing.Point(569, 3);
+            this.btnClear.Location = new System.Drawing.Point(964, 3);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(87, 59);
+            this.btnClear.Size = new System.Drawing.Size(116, 67);
             this.btnClear.TabIndex = 4;
             this.toolTip1.SetToolTip(this.btnClear, "Delete Selected File");
             this.btnClear.UseVisualStyleBackColor = false;
@@ -120,8 +124,8 @@
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 88.79082F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.20918F));
             this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.btnMerge, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.btnClear, 1, 0);
@@ -136,14 +140,14 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1133, 651);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1083, 732);
             this.tableLayoutPanel1.TabIndex = 5;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1133, 651);
+            this.ClientSize = new System.Drawing.Size(1083, 732);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
